b9d440fc39b755fed32782d137b307845ffd18f8
VineeAndre <84481899+VineeAndre@users.noreply.github.com>
2025-08-23 12:07:40 -0300

    versel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/meu-repo/', // Remover para deploy no Vercel
+  base: '/meu-repo/', // se for GitHub Pages
+  optimizeDeps: {
+    include: ['aos'] // <- força a inclusão do AOS no build
+  }
 });
